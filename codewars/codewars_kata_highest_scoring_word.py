893dbffafbfc6c9847abd2fb81af6a9b162f29eb
Alessandro <alessandropadilharocha@gmail.com>
2026-01-04 19:12:50 +0000

    study_2026_01_04

--- codewars/codewars_kata_highest_scoring_word.py
+++ codewars/codewars_kata_highest_scoring_word.py
@@ -1,3 +1,10 @@
 def high(x):
-    list_words = ' '.split(x)
-    
+    list_words = x.split()
+    list_score = [sum((ord(char) - ord('a') + 1) for char in word) for word in list_words]
+    return list_words[list_score.index(max(list_score))]
+
+
+#Higher-Order-Functions
+#Solução utilizando função com o parâmetro key
+def high(x):
+    return max(x.split(), key = lambda word: sum(ord(c) - ord('a') + 1 for c in word))
